dae40eabe04d80a5b199b299e63e33fbd19e8ecc
rissem <rissem@gmail.com>
2014-02-11 15:34:55 -0800

    fix it/before/beforeAll etc to play nice w/ fibers

--- serverPreTest.js
+++ serverPreTest.js
@@ -1,4 +1,52 @@
 var Mocha = Npm.require("mocha");
+var Fiber = Npm.require("fibers");
+var _ = Npm.require("underscore");
+
+//basically a direct copy from meteor/packages/meteor/dynamics_nodejs.js
+//except the wrapped function has an argument (mocha distinguishes
+//asynchronous tests from synchronous ones by the "length" of the
+//function passed into it, before, etc.)
+var moddedBindEnvironment = function (func, onException, _this) {
+  if (!Fiber.current)
+    throw new Error(noFiberMessage);
+
+  var boundValues = _.clone(Fiber.current._meteor_dynamics || []);
+
+  if (!onException || typeof(onException) === 'string') {
+    var description = onException || "callback of async function";
+    onException = function (error) {
+      Meteor._debug(
+        "Exception in " + description + ":",
+        error && error.stack || error
+      );
+    };
+  }
+
+  //note the callback variable present here
+  return function (callback) {
+    var args = _.toArray(arguments);
+
+    var runWithEnvironment = function () {
+      var savedValues = Fiber.current._meteor_dynamics;
+      try {
+        // Need to clone boundValues in case two fibers invoke this
+        // function at the same time
+        Fiber.current._meteor_dynamics = _.clone(boundValues);
+        var ret = func.apply(_this, args);
+      } catch (e) {
+        onException(e);
+      } finally {
+        Fiber.current._meteor_dynamics = savedValues;
+      }
+      return ret;
+    };
+
+    if (Fiber.current)
+      return runWithEnvironment();
+    Fiber(runWithEnvironment).run();
+  };
+};
+
 
 //Mocha Base Reporter
 var Base = Npm.require("mocha/lib/reporters/base");
@@ -23,7 +71,6 @@ function MeteorCollectionTestReporter(runner){
     return MochaWebTestReports.find();
   });
 
-
   function saveTestResult(test){
     function getParents(node, parents){
       if (!node.parent || node.parent.title === ""){
@@ -37,6 +84,7 @@ function MeteorCollectionTestReporter(runner){
     var err = null
     if (test.err){
       err = {message: test.err.message, stack: test.err.stack};
+      console.log(err.message, err.stack);
     }
 
     MochaWebTests.insert({
@@ -89,22 +137,52 @@ function MeteorCollectionTestReporter(runner){
   }));
 }
 
-mocha = new Mocha({ui: "bdd", reporter: MeteorCollectionTestReporter});
+
 var mochaExports = {};
 //this line retrieves the describe, it, etc. functions and puts them
-//into mochaExports
+//into mochaExports (mochaExports = {it: func, desc: func,...})
+mocha = new Mocha({ui: "bdd", reporter: MeteorCollectionTestReporter});
 mocha.suite.emit("pre-require", mochaExports);
 //console.log(mochaExports);
 
-//patch up test description functions so they play nice w/ fibers
-//TODO should export other test description functions here as well
+//patch up describe function so it plays nice w/ fibers
 describe = function (name, func){
   mochaExports.describe(name, Meteor.bindEnvironment(func, function(err){throw err; }));
 };
 
-it = function (name, func){
-  mochaExports.it(name, Meteor.bindEnvironment(func, function(err){throw err; }));
-};
+//In Meteor, these blocks will all be invoking Meteor code and must
+//run within a fiber. We must therefore wrap each with something like
+//bindEnvironment. The function passed off to mocha must have length
+//greater than zero if we want mocha to run it asynchronously. That's
+//why it uses the moddedBindEnivronment function described above instead
+
+//We're actually having mocha run all tests asynchronously. This
+//is because mocha cannot tell when a synchronous fiber test has
+//finished, because the test runner runs outside a fiber.
+
+//It is possible that the mocha test runner could be run from within a
+//fiber, but it was unclear to me ow that could be done without
+//forking mocha itself.
+
+["it", "before", "beforeEach", "after", "afterEach"].forEach(function(testFunctionName){
+  global[testFunctionName] = function (name, func){
+    wrappedFunc = function(callback){
+      if (func.length == 0){
+        func();
+        callback();
+      }
+      else {
+        func(callback);
+      }
+    }
+
+    boundWrappedFunction = moddedBindEnvironment(wrappedFunc, function(err){
+      throw err;
+    });
+
+    mochaExports[testFunctionName](name, boundWrappedFunction);
+  }
+});
 
 Meteor.startup(function(){
   mocha.run();
