c2e0a71dc5368a50fa2de931b89f6a62c0d4adc9
rissem <rissem@gmail.com>
2014-06-16 10:25:25 -0700

    fix a race condition
    reporter was sometimes set after tests were run
    update npm shrinkwrap

--- client.js
+++ client.js
@@ -3,27 +3,24 @@
 ddpParentConnection = null;
 window.mochaWebClientTestsComplete = false;
 
+var testSetupFunctions = []
+
 MochaWeb.testOnly = function(callback){
-  //this could be cached on the client..
-  Meteor.call("mirrorInfo", function(error, mirrorInfo){
-    if (mirrorInfo.isMirror){
-      ddpParentConnection = DDP.connect(mirrorInfo.parentUrl);
-      //TODO allow ui to be customized with Meteor.settings
-      mocha.setup({reporter: MochaWeb.MeteorCollectionTestReporter, ui: "bdd"});
-      callback();
-    }
-    if (error){
-      console.error(error);
-    }
-  })
+  testSetupFunctions.push(callback);
 };
 
 Meteor.call("mirrorInfo", function(error, mirrorInfo){
   if (mirrorInfo.isMirror){
     Meteor.setTimeout(function(){
-      // console.log("RUN THE CLIENT TESTS");
+      ddpParentConnection = DDP.connect(mirrorInfo.parentUrl);
+      //TODO allow ui to be customized with Meteor.settings
+      mocha.setup({reporter: MochaWeb.MeteorCollectionTestReporter, ui: "bdd"});
+      testSetupFunctions.forEach(function(testFunction){
+        testFunction();
+      });
       mocha.run(function(){
         window.mochaWebClientTestsComplete = true;
+        //TODO INVOKE CALLBACK HERE
       });
     }, 0);
   }
