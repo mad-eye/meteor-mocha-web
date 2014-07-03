8645314e6883a75a752118e4468bbdb11bbfcc98
rissem <rissem@gmail.com>
2014-07-03 15:08:28 -0700

    fix sample tests

--- sample-tests/server.js
+++ sample-tests/server.js
@@ -1,7 +1,9 @@
-MochaWeb.testOnly(function(){
-  describe("Server initialization", function(){
-    it("should have a Meteor version defined", function(){
-      chai.assert(Meteor.release);
+if (!(typeof MochaWeb === 'undefined')){
+  MochaWeb.testOnly(function(){
+    describe("Server initialization", function(){
+      it("should have a Meteor version defined", function(){
+        chai.assert(Meteor.release);
+      });
     });
   });
-});
+}
