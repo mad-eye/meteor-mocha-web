60b60a22821c9de3f6935db0a25cf10aa12b533d
rissem <rissem@gmail.com>
2014-07-03 14:42:48 -0700

    add sample tests

---
+++ sample-tests/server.js
@@ -0,0 +1,7 @@
+MochaWeb.testOnly(function(){
+  describe("Server initialization", function(){
+    it("should have a Meteor version defined", function(){
+      chai.assert(Meteor.release);
+    });
+  });
+});
