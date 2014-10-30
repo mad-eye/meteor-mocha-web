76ebfc69a1f90ff40da533d2dae8b97b7955f7f2
rissem <rissem@gmail.com>
2014-10-30 10:10:46 -0700

    point to latest html reporter

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mike:mocha',
   summary: "Run mocha tests in the browser",
-  version: "0.4.1",
+  version: "0.4.2",
   debugOnly: true,
   git: "https://github.com/mad-eye/meteor-mocha-web"
 });
