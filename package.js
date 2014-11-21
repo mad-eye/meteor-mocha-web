f6cedb30c71a61358392f2179d1e181e91705946
rissem <rissem@gmail.com>
2014-11-21 10:38:19 -0800

    depend on velocity:core@0.3.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mike:mocha-package',
   summary: "Run mocha tests in the browser",
-  version: "0.4.5",
+  version: "0.4.6",
   debugOnly: true,
   git: "https://github.com/mad-eye/meteor-mocha-web"
 });
@@ -18,7 +18,7 @@ Npm.depends({
 
 Package.on_use(function (api, where) {
   api.use(['underscore@1.0.1'], ['server', 'client']);
-  api.use(['velocity:core@1.0.0-rc.1'], "server");
+  api.use(['velocity:core@0.3.0'], "server");
   api.use(['mike:html-reporter@0.3.1'], "client");
   api.use(['templating@1.0.6'], "client");
   api.use(['velocity:shim@0.0.1'], ["client", "server"]);
