b48215215fd89c7f221005489dcea259b7743fd8
sam@hatoum.net <sam@hatoum.net>
2014-12-21 18:02:51 -0800

    bumping versions

--- package.js
+++ package.js
@@ -21,7 +21,7 @@ Package.on_use(function (api, where) {
   api.use(['velocity:html-reporter@0.3.2'], "client");
   api.use(['templating@1.0.6'], "client");
   api.use(['velocity:shim@0.1.0'], ["client", "server"]);
-  api.use("velocity:node-soft-mirror@0.2.3", {unordered: true});
+  api.use("velocity:node-soft-mirror@0.2.4", {unordered: true});
 
   api.add_files(["reporter.js", "server.js"], "server");
   api.add_files(["client.html", "mocha.js", "reporter.js", "client.js", "chai.js"], "client");
