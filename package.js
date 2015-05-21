4cb184943758cc3aa430e69093eeb60bc658eb5c
Mike Risse <rissem@gmail.com>
2015-05-20 18:56:34 -0700

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mike:mocha-package',
   summary: "Run mocha tests in the browser",
-  version: "0.5.7",
+  version: "0.5.8",
   git: "https://github.com/mad-eye/meteor-mocha-web"
 });
 
